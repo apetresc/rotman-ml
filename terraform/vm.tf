9d27dd4fdf4f58698d68f0aec535538e92a465fd
Adrian Petrescu <adrian@apetre.sc>
2023-02-06 23:37:36 -0500

    Add example terraform config for a blank VM

---
+++ terraform/vm.tf
@@ -0,0 +1,29 @@
+resource "google_compute_instance" "default" {
+  name         = "test"
+  project      = "anthos-poc-315613"
+  machine_type = "e2-standard-2"
+  zone         = "northamerica-northeast1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  service_account {
+    email = "92657199920-compute@developer.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring.write",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/service.management.readonly",
+    "https://www.googleapis.com/auth/trace.append"]
+  }
+}
